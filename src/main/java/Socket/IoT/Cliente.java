1bbfb020419518fab0b9b4253959e2903d843f73
ICARO SANTANA SILVA <100904037@ucsal.br>
2020-03-09 12:35:54 -0300

    alteracoes para adaptar outras linguagens

--- src/main/java/Socket/IoT/Cliente.java
+++ src/main/java/Socket/IoT/Cliente.java
@@ -3,13 +3,13 @@ package Socket.IoT;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.SocketException;
 import java.util.Scanner;
 
 public class Cliente {
-
 	public static void main(String args[]) {
 		try {
 			String host = null;
@@ -24,18 +24,38 @@ public class Cliente {
 				Socket socket = new Socket(host, porta);
 				BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 				PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-	
-				System.out.println("SERVER_LOG: " + br.readLine());
-	
-				BufferedReader userInputBR = new BufferedReader(new InputStreamReader(System.in));
-				String userInput = userInputBR.readLine();
 				
-				if ("bye".equalsIgnoreCase(userInput)) {
-					socket.close();
-					break;
+				ObjectInputStream entrada = new ObjectInputStream(socket.getInputStream());
+				String mensagemDoServidor = null;
+				try {
+					mensagemDoServidor = (String) entrada.readObject();
+				} catch (ClassNotFoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				System.out.println(mensagemDoServidor);
+//				if(br.readLine() != null)
+//					System.out.println("SERVER_LOG: " + br.readLine());
+	
+//				BufferedReader userInputBR = new BufferedReader(new InputStreamReader(System.in));
+//				String userInput = userInputBR.readLine();
+				
+				Scanner sc2 = new Scanner(socket.getInputStream());
+				while (sc2.hasNextLine()) {
+//					System.out.println(sc.nextLine());
+					out.flush();
+					out.println(sc.nextLine() + "\r\n");
 				}
 				
-				out.println(userInput);
+				
+//				if ("bye".equalsIgnoreCase(userInput)) {
+//					socket.close();
+//					break;
+//				}
+				
+//				out.flush();
+//				out.println(userInput + "\r\n" + "\r\n");
+//				out.print("\r\n");
 		
 				System.out.println("SERVER_LOG: " + br.readLine());
 			}
--- src/main/java/Socket/IoT/Servidor.java
+++ src/main/java/Socket/IoT/Servidor.java
@@ -3,31 +3,51 @@ package Socket.IoT;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.ObjectOutputStream;
 import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Servidor {
-
+// OK - JESSICA{Cliente}
 	public static void main(String args[]) throws IOException {
-		final int porta = 3322;
-		System.out.println("Server alocado na porta: " + porta);
-		ServerSocket serverSocket = new ServerSocket(porta);
-		while (true) {
-			Socket socket = serverSocket.accept();
-			OutputStream os = socket.getOutputStream();
-			PrintWriter pw = new PrintWriter(os, true);
-			pw.println("Qual seu nome para, ..Ping?");
-
-			BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-			String str = br.readLine();
-
-			pw.println("Lá vai, " + str);
-//			pw.close();
-//			socket.close();
-
-			System.out.println("Pong: " + str);
+		try {
+			final int porta = 3322;
+			System.out.println("Server alocado na porta: " + porta);
+			ServerSocket serverSocket = new ServerSocket(porta);
+			while (serverSocket.isBound()) {
+				Socket socket = serverSocket.accept();
+				
+				System.out.println("Cliente conectado: " + socket.getInetAddress().getHostAddress());
+				
+				ObjectOutputStream mensagemServidor = new ObjectOutputStream(socket.getOutputStream());
+				mensagemServidor.flush();
+				mensagemServidor.writeObject("ObjectOutputStream method");
+				
+				OutputStream os = socket.getOutputStream();
+				PrintWriter pw = new PrintWriter(os, true);
+				pw.println("Qual seu nome para, ..Ping?");
+	
+//				BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+//				String str = br.readLine();
+				
+				Scanner sc = new Scanner(socket.getInputStream());
+				while (sc.hasNextLine()) {
+					System.out.println(sc.nextLine());					
+				}
+				
+//				System.out.println("msg: " + str);
+				
+//				pw.println("Lá vai, " + str);
+//				pw.close();
+//				socket.close();
+//				System.out.println("Pong: " + str);
+			}
+		} catch (Exception e) {
+			System.out.println(e.getLocalizedMessage());
+			// TODO: handle exception
 		}
 	}
 }
